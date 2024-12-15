763e585043d65e62e3fe85adc7cc380258544ba5
Ruby <93859705+MisterRuby@users.noreply.github.com>
2024-12-15 10:17:36 +0900

    chore : 프로젝트 생성

---
+++ src/main/kotlin/ruby/testsubmoduleother/TestSubmoduleOtherApplication.kt
@@ -0,0 +1,11 @@
+package ruby.testsubmoduleother
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestSubmoduleOtherApplication
+
+fun main(args: Array<String>) {
+    runApplication<TestSubmoduleOtherApplication>(*args)
+}
---
+++ src/test/kotlin/ruby/testsubmoduleother/TestSubmoduleOtherApplicationTests.kt
@@ -0,0 +1,13 @@
+package ruby.testsubmoduleother
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestSubmoduleOtherApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
